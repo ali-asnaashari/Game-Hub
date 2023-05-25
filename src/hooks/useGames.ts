4f0f31759ee6578dd0e98b2c52de26041c1903a9
Ali Asnaashari <79118507+ali-asnaashari@users.noreply.github.com>
2023-05-25 19:07:26 +0330

    Show Loading Skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -28,23 +28,29 @@ const useGames = () => {
 
     // Storing Game Objects
     const [games, setGames] = useState<Game[]>([]);
-
     // Error Messages
     const [error, setError] = useState('');
+    // Loading Skeletons
+    const [isLoading, setLoading] = useState(false);
 
     // Send Fetch Request to Backend
     useEffect(() => {
+        setLoading(true);
         apiClient.get<FetchGameResponse>('/games', {signal: controller.signal})
-            .then(res => setGames(res.data.results))
+            .then(res => {
+                setGames(res.data.results);
+                setLoading(false);
+            })
             .catch(err => {
                 if (err instanceof CanceledError) return;
                 setError(err.message);
+                setLoading(false);
             });
 
         return () => controller.abort();
     }, []);
 
-    return {games, error};
+    return {games, error,isLoading};
 }
 
 export default useGames;
