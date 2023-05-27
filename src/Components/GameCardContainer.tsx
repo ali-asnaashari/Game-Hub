e5619161792f6e5445e25ec94cdc5aa494cb9026
Ali Asnaashari <79118507+ali-asnaashari@users.noreply.github.com>
2023-05-27 21:09:29 +0330

    Display Genres

--- src/Components/GameCardContainer.tsx
+++ src/Components/GameCardContainer.tsx
@@ -9,7 +9,7 @@ const GameCardContainer = ({children}: Props) => {
 
     return (
         <>
-            <Box width='300px' borderRadius={10} overflow="hidden">
+            <Box borderRadius={10} overflow="hidden">
                 {children}
             </Box>
         </>
