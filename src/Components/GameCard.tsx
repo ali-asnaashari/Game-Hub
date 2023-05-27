5c31b7191777abf29ae2480435c7c8fae8b71307
Ali Asnaashari <79118507+ali-asnaashari@users.noreply.github.com>
2023-05-27 21:28:45 +0330

    Show a Spinner while Fetching Genres

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -15,7 +15,7 @@ const GameCard = ({game}: Props) => {
         <Card>
             <Image src={getCroppedImageUrl(game.background_image)}></Image>
             <CardBody>
-                <Heading fontSize='2xl'>{game.name}</Heading>
+                <Heading fontSize='1xl' paddingBottom={2}>{game.name}</Heading>
                 <HStack justifyContent='space-between'>
                     <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
                     <CriticScore score={game.metacritic}/>
--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,15 +1,22 @@
 import useGenres from "../hooks/useGenres";
-import {HStack, Image, List, ListItem, Text} from "@chakra-ui/react";
+import {HStack, Image, List, ListItem, Spinner, Text} from "@chakra-ui/react";
 import getCroppedImageUrl from "../Services/image-url";
 
 const GenreList = () => {
-    const {data} = useGenres();
+    const {data, isLoading, error} = useGenres();
+
+    if (error)
+        return null;
+
+    if (isLoading)
+        return <Spinner size='md' thickness='4px' speed='0.65s' emptyColor='gray.200' color='blue.500'/>
+
     return (
         <List>
             {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
                 <HStack>
-                    <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-                    <Text fontSize='lg' >{genre.name}</Text>
+                    <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
+                    <Text fontSize='lg'>{genre.name}</Text>
                 </HStack>
             </ListItem>)}
         </List>
